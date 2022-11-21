3a09743304ae6196652a75db91b93fdb02b68a36
MartiHr <81310884+MartiHr@users.noreply.github.com>
2022-11-21 20:55:49 +0200

    Create LoginViewModel.cs

---
+++ Web/Epsilon.Web.ViewModels/ApplicationUser/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Epsilon.Web.ViewModels.ApplicationUser
+{
+    public class LoginViewModel
+    {
+        // TODO: Finish implementing the viewModel: add properties and other things if needed (for example - email)
+        [Required]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
