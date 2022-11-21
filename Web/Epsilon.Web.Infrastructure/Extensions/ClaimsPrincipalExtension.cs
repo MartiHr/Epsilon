1608b517cdc767d33912ef3a9c540abbbcf93b7c
MartiHr <81310884+MartiHr@users.noreply.github.com>
2022-11-21 20:56:06 +0200

    Added claims principal extension

---
+++ Web/Epsilon.Web.Infrastructure/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace Epsilon.Web.Infrastructure.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}
