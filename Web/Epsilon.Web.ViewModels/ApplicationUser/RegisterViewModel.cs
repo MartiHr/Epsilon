ce1ea2de463edb54bb27dfa5cc62c3f6a4b9a25b
MartiHr <81310884+MartiHr@users.noreply.github.com>
2022-11-21 20:55:44 +0200

    Create RegisterViewModel.cs

---
+++ Web/Epsilon.Web.ViewModels/ApplicationUser/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Epsilon.Web.ViewModels.ApplicationUser
+{
+    public class RegisterViewModel
+    {
+        // TODO: Finish implementing the viewModel: add properties and other things if needed
+
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(60, MinimumLength = 10)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password and ConfirmPassword do not match")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
