f99390e31fd4b89dff2b467473c0393efef988eb
MartiHr <81310884+MartiHr@users.noreply.github.com>
2022-11-28 16:25:24 +0200

    Finished CategoryDropdownViewModel

--- Web/Epsilon.Web.ViewModels/Category/CategoryDropdownView.cs
+++ Web/Epsilon.Web.ViewModels/Category/CategoryDropdownView.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Epsilon.Web.ViewModels.Category
+﻿namespace Epsilon.Web.ViewModels.Category
 {
     public class CategoryDropdownView
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
     }
 }
