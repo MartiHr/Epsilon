33b90fad54e02d00d2709278ef3e485b6a6668ea
MartiHr <81310884+MartiHr@users.noreply.github.com>
2022-12-07 18:14:40 +0200

    Added ComputerEditInputModel

---
+++ Web/Epsilon.Web.ViewModels/Computer/ComputerEditInputModel.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using AutoMapper;
+using Epsilon.Common;
+using Epsilon.Services.Mapping;
+using Epsilon.Web.ViewModels.Category;
+using Epsilon.Web.ViewModels.Manufacturer;
+using Epsilon.Web.ViewModels.Part;
+using Microsoft.AspNetCore.Http;
+
+using static Epsilon.Data.Common.DataValidation.Computer;
+
+using ComputerModel = Epsilon.Data.Models.Computer;
+
+namespace Epsilon.Web.ViewModels.Computer
+{
+    public class ComputerEditInputModel : IMapFrom<ComputerModel>
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [StringLength(ComputerNameMaxLength, MinimumLength = ComputerNameMinLength)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(ComputerModelMaxLength, MinimumLength = ComputerModelMinLength)]
+        public string Model { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), ComputerPriceMinValue, ComputerPriceMaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(ComputerDescriptionMaxLength, MinimumLength = ComputerDescriptionMinLength)]
+        public string Description { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<CategoryDropdownViewModel> Categories { get; set; } = new List<CategoryDropdownViewModel>();
+
+        [Required]
+        public int ManufacturerId { get; set; }
+
+        public IEnumerable<ManufacturerDropdownViewModel> Manufacturers { get; set; } = new List<ManufacturerDropdownViewModel>();
+
+        [Required]
+        public int CPUId { get; set; }
+
+        public IEnumerable<PartDropdownViewModel> CPUs { get; set; } = new List<PartDropdownViewModel>();
+
+        [Required]
+        public int GPUId { get; set; }
+
+        public IEnumerable<PartDropdownViewModel> GPUs { get; set; } = new List<PartDropdownViewModel>();
+
+        [Required]
+        public int StorageId { get; set; }
+
+        public IEnumerable<PartDropdownViewModel> Storages { get; set; } = new List<PartDropdownViewModel>();
+
+        [AllowedExtensionsForCollection(new string[] { GlobalConstants.PNGExtension, GlobalConstants.JPGExtension, GlobalConstants.JPEGExtension })]
+        public ICollection<IFormFile> Imagess { get; set; }
+    }
+}
