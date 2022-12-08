9ec687adf621d5c1d0afe9def539e8721ff63c0e
MartiHr <81310884+MartiHr@users.noreply.github.com>
2022-12-08 21:49:27 +0200

    Create CategoryListViewModel.cs

---
+++ Web/Epsilon.Web.ViewModels/Category/CategoryListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Epsilon.Web.ViewModels.Category
+{
+    public class CategoryListViewModel
+    {
+        //// TODO: implement paging
+
+        public IEnumerable<CategoryInListViewModel> Computers { get; set; } = new List<CategoryInListViewModel>();
+    }
+}
