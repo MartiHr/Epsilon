9ec0dc5992a7358c9cf5d5eb2e44cd036b77dfe0
MartiHr <81310884+MartiHr@users.noreply.github.com>
2022-12-08 21:49:32 +0200

    Create CategoryInListViewModel.cs

---
+++ Web/Epsilon.Web.ViewModels/Category/CategoryInListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AutoMapper;
+using Epsilon.Services.Mapping;
+
+using CategoryModel = Epsilon.Data.Models.Category;
+
+namespace Epsilon.Web.ViewModels.Category
+{
+    public class CategoryInListViewModel : IMapFrom<CategoryModel>, IHaveCustomMappings
+    {
+        public string Name { get; set; }
+
+        public int ComputersCount { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<CategoryModel, CategoryInListViewModel>()
+              .ForMember(x => x.ComputersCount, opt =>
+                  opt.MapFrom(c => c.Computers.Count));
+        }
+    }
+}
