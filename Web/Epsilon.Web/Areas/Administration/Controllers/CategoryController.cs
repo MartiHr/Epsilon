dd9e75387a81ac3c0080a2ce816f07ef94b98e8b
MartiHr <81310884+MartiHr@users.noreply.github.com>
2022-12-08 21:49:14 +0200

    Create CategoryController.cs

---
+++ Web/Epsilon.Web/Areas/Administration/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+using Epsilon.Web.ViewModels.Category;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Epsilon.Web.Areas.Administration.Controllers
+{
+    public class CategoryController : AdministrationController
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryCreateInputModel model)
+        {
+            return View();
+        }
+    }
+}
