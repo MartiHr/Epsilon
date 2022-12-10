e404327b4e8c19be2ddbc2bf82f4874bacfafdf2
MartiHr <81310884+MartiHr@users.noreply.github.com>
2022-12-10 19:38:42 +0200

    Created ManufactureListViewModel

---
+++ Web/Epsilon.Web.ViewModels/Manufacturer/ManufacturerListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Epsilon.Web.ViewModels.Manufacturer
+{
+    public class ManufacturerListViewModel
+    {
+        //// TODO: implement paging
+
+        public IEnumerable<ManufacturerInListViewModel> Manufacturers { get; set; } = new List<ManufacturerInListViewModel>();
+    }
+}
