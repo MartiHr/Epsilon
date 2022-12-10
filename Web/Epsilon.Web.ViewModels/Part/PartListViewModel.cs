bb8bf2bb2a643ae0a19c7a37410165b029425fbb
MartiHr <81310884+MartiHr@users.noreply.github.com>
2022-12-10 20:58:38 +0200

    Create PartListViewModel.cs

---
+++ Web/Epsilon.Web.ViewModels/Part/PartListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Epsilon.Web.ViewModels.Part
+{
+    public class PartListViewModel
+    {
+        public IEnumerable<PartInListViewModel> Parts { get; set; } = new List<PartInListViewModel>();
+    }
+}
